cf284b55a64aa99f3d80273d278c06f8138afcf4
badillosoft <badillo.soft@hotmail.com>
2020-01-27 12:20:55 -0600

    dragon v2 200129.1220

---
+++ v2/lambda.js
@@ -0,0 +1,70 @@
+console.log("Lambda Cloud - Mini v1.0");
+
+async function lambda(groupID, nodeID, masterURI = "https://badillosoft.com") {
+    if (!window.io) {
+        async function installScript(url) {
+            const script = document.createElement("script");
+            await new Promise((resolve, reject) => {
+                script.src = url;
+                script.addEventListener("load", resolve);
+                script.addEventListener("error", reject);
+                document.body.append(script);
+            });
+        }
+        await installScript("https://cdnjs.cloudflare.com/ajax/libs/node-uuid/1.4.8/uuid.js");
+        await installScript("https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js");
+    }
+
+    const instance = {
+        socketMaster: null,
+        socketHouse: null,
+        connectedMaster: false,
+        registeredMaster: false,
+        node: {
+            groupID,
+            nodeID
+        },
+        async connectMaster() {
+            if (this.socketMaster) this.socketMaster.disconnect();
+            this.socketMaster = io(masterURI);
+            await new Promise(resolve => {
+                this.socketMaster.on("connect", () => {
+                    console.log(`Connected to ${masterURI}`);
+                    this.connectedMaster = true;
+                    this.socketMaster.emit("node", this.node, async node => {
+                        console.log("Signed successfully:", node.groupToken.slice(24));
+                        this.node = node;
+                        this.registeredMaster = true;
+                        resolve();
+                    });
+                });
+                this.socketMaster.on("disconnect", () => {
+                    this.connectedMaster = false;
+                    console.warn("disconnected master");
+                });
+                this.socketMaster.on("reset", async () => {
+                    console.warn("reset house");
+                    await this.connectHouse();
+                });
+            });
+        },
+        async to(nodeTo, code) {
+            return await new Promise(resolve => {
+                this.socketMaster.emit("peer", {
+                    ...this.node,
+                    protocol: {
+                        type: "PEER"
+                    },
+                    code: JSON.stringify(code),
+                }, { nodeID: nodeTo }, output => {
+                    console.log("peer success", output);
+                    resolve(output);
+                });
+            });
+        }
+    };
+
+    await instance.connectMaster();
+
+    return instance;
+}
